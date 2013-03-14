01fa7eacde3495250663c291e3d8abadc3f1bc56
eattherich <christian_roepke@hotmail.com>
2013-03-14 15:58:21 +0100

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CriticalNetwork::Application.config.secret_token = '0c4255bb35b9f68a12aa56bc330f41ac2f0b1ef2175ac597e821b009b86f09f404a86fff64a759c9b2aa6885fd6060427d5fe823d55bf1da35e26a86917e9039'
